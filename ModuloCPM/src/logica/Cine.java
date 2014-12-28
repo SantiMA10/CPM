74a6ad18a839608bae05f4c8b5615300bff9613e
santiagomartin <santiagomartin@10.0.0.11>
2014-12-28 19:36:21 +0100

    Adding basic stuff to Logica classes

--- ModuloCPM/src/logica/Cine.java
+++ ModuloCPM/src/logica/Cine.java
@@ -1,5 +1,75 @@
 package logica;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Cine {
+	private Cartelera cartelera;
+	private String nombre;
+	private double precioEntrada;
+	private double descuentoJubilado;
+	private double incrementoCumple;
+	private double incremento3D;
+	
+	public Cine(){
+		loadCine();
+		setCartelera(new Cartelera());
+	}
+	public void loadCine(){
+		String linea = "";
+	    try {
+	    	BufferedReader fichero = new BufferedReader(new FileReader("files/precios.dat"));
+	    	while (fichero.ready()) {
+	    		if(!linea.isEmpty()){
+	    			linea = fichero.readLine();
+		    		String[] datos = linea.split(";");
+		    		setNombre(datos[0]);
+		    		setPrecioEntrada(Double.parseDouble(datos[1]));
+		    		setDescuentoJubilado(Double.parseDouble(datos[2]));
+		    		setIncrementoCumple(Double.parseDouble(datos[3]));
+		    		setIncremento3D(Double.parseDouble(datos[4]));
+	    		}
+	    	}
+	    	fichero.close();
+	    	}catch (Exception e) {
+	    		e.printStackTrace();
+	    	}
+	}
+	public Cartelera getCartelera() {
+		return cartelera;
+	}
+	private void setCartelera(Cartelera cartelera) {
+		this.cartelera = cartelera;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecioEntrada() {
+		return precioEntrada;
+	}
+	private void setPrecioEntrada(double precioEntrada) {
+		this.precioEntrada = precioEntrada;
+	}
+	public double getDescuentoJubilado() {
+		return descuentoJubilado;
+	}
+	private void setDescuentoJubilado(double descuentoJubilado) {
+		this.descuentoJubilado = descuentoJubilado;
+	}
+	public double getIncrementoCumple() {
+		return incrementoCumple;
+	}
+	private void setIncrementoCumple(double incrementoCumple) {
+		this.incrementoCumple = incrementoCumple;
+	}
+	public double getIncremento3D() {
+		return incremento3D;
+	}
+	private void setIncremento3D(double incremento3d) {
+		incremento3D = incremento3d;
+	}
 
 }
